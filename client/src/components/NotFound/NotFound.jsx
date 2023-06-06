e1e4fb0d13416c0c613a338b087d24180e113c7b
johalama2023 <johalama2023@gmail.com>
2023-06-06 16:13:58 -0500

    mejorando

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import notFound from '../../assets/img/404NotFound.png'
+import { useNavigate } from "react-router-dom";
 import './NotFound.css'
 
 const NotFound = () => {
+
+  const navigate = useNavigate()
+
+  const handleGoHome = () => {
+    navigate('/pokemon')
+  }
+
   return (
     <div className='notFound__container'>
         <img className='notFound__img' src={notFound} alt="" />
+        <button className='notFound__btn' onClick={handleGoHome}>Go Home...</button>
     </div>
   )
 }
